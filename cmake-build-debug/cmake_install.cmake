f488b01977b486f2c35ce83366e0403d2a94d240
YuvalRashman <56125252+YuvalRashman@users.noreply.github.com>
2022-10-31 22:19:42 +0200

    fix

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/yrash/CLionProjects/C_LIB
+# Install script for directory: C:/Users/yrash/GitHub/C_LIBARY
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/yrash/CLionProjects/C_LIB/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/yrash/GitHub/C_LIBARY/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
