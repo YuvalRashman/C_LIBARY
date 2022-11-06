feef72a796f3616ccbb61af9b4de47d6c96f3f91
YuvalRashman <56125252+YuvalRashman@users.noreply.github.com>
2022-11-06 19:36:46 +0200

    improve game

--- TicTacToe.c
+++ TicTacToe.c
@@ -1,47 +1,50 @@
 //
-// Created by yrash on 11/6/2022.
+// Created by Yuval Rashman on 11/6/2022.
 //
 
 #include <stdio.h>
 #include "definesLib.h"
 
-#define SignToBool(sign) sign == 'X' ? 1 : 0
+#define X_SIGN 'X'
+#define O_SIGN 'O'
+#define NONE_SIGN '-'
+#define DIMENSION_SIZE 2
+#define BOARD_LEN 3
+#define TURNS_TO_SIGN_BOOL(turn)  IS_ODD(turns)
+#define POS_IN_MASK(row, col) (row * BOARD_LEN + col)
+#define POS_IN_MASK_TO_BITS(row, col) (ONE << POS_IN_MASK(row, col))
+#define BOOL_TO_SIGN(bool) bool ? X_SIGN : O_SIGN
 
-unsigned short markPos(unsigned short *hasMask, unsigned short *xOrOMask, unsigned short pos[2]) {
-    *hasMask |= (1 << (pos[0] * 3 + pos[1]));
-    *xOrOMask |= (1 << (pos[0] * 3 + pos[1]));
-}
-
-void printXorO(BOOL xOrO) { // always 1
-    (xOrO > 0) ? printf("X ") : printf(" O");
+void printXorO(BOOL xOrO) {
+    xOrO ? printf("%c", X_SIGN) : printf("%c", O_SIGN);
 }
 
 void printNone() {
-    printf("-");
+    printf("%c",NONE_SIGN);
 }
 
 void printRow(unsigned short hasMask, unsigned short xOrOMask, unsigned short row) {
-    unsigned short colume;
-    unsigned short pos;
-    unsigned temp;
-    for (colume = 0; colume < 3; colume++) {
-        pos = (1 << (row * 3 + colume));
-        hasMask & pos ? printXorO((xOrOMask & pos) != 0) : printNone();
+    unsigned short col;
+    unsigned short posInBits;
+    for (col = ZERO; col < BOARD_LEN; col++) {
+        posInBits = POS_IN_MASK_TO_BITS(row, col);
+        hasMask & posInBits ? printXorO((xOrOMask & posInBits) != ZERO) : printNone();
     }
+    printf("\n");
 }
 
 void printBoard(unsigned short hasMask, unsigned short xOrOMask) {
     unsigned short row;
-    for (row = 0; row < 3; row++) {
+    for (row = ZERO; row < BOARD_LEN; row++) {
         printRow(hasMask, xOrOMask, row);
-        printf("\n");
     }
 }
 
 BOOL isRowOrColSame(unsigned short hasMask, unsigned short xOrOMask, BOOL dimension, unsigned short pos) {
-    unsigned short realPose = 3 * pos;
-    unsigned short num = dimension ? 7 : 0x49;
-    return (((hasMask >> realPose) & num) == num) * (!(((xOrOMask >> realPose) & num) % num));
+    unsigned short bitsToMove = BOARD_LEN * pos;
+    unsigned short validateBits = dimension ? 7 : 0x49;
+    return (((hasMask >> bitsToMove) & validateBits) == validateBits) * (!(((xOrOMask >> bitsToMove) & validateBits) %
+                                                                           validateBits));
 }
 
 BOOL isDiagSame(unsigned short hasMask, unsigned short xOrOMask, BOOL mainOrSecond) {
@@ -50,37 +53,41 @@ BOOL isDiagSame(unsigned short hasMask, unsigned short xOrOMask, BOOL mainOrSeco
 }
 
 BOOL isWinner(unsigned short hasMask, unsigned short xOrOMask) {
-    BOOL flag = isDiagSame(hasMask, xOrOMask, 1) || isDiagSame(hasMask, xOrOMask, 0);
+    BOOL flag = isDiagSame(hasMask, xOrOMask, ONE) || isDiagSame(hasMask, xOrOMask, ZERO);
     unsigned short offset = 0;
     while (offset < 3 && !flag) {
-        flag = isRowOrColSame(hasMask, xOrOMask, 0, offset) ||
-               isRowOrColSame(hasMask, xOrOMask, 1, offset++);
+        flag = isRowOrColSame(hasMask, xOrOMask, ZERO, offset) ||
+               isRowOrColSame(hasMask, xOrOMask, ONE, offset++);
     }
     return flag;
 }
 
-void runGame() {
-    unsigned short hasMask = 0;
-    unsigned short xOrOMask = 0;
-    unsigned short pos[2] = {0};
-    BOOL win = 0;
-    BOOL sign = 'X';
+void runTicTacToeGame() {
+    unsigned short hasMask = ZERO;
+    unsigned short xOrOMask = ZERO;
+    unsigned short pos[DIMENSION_SIZE];
+    BOOL win = ZERO;
+    unsigned short turns = NINE;
     printBoard(hasMask, xOrOMask);
-    while (!win) {
-        printf("enter row:");
-        scanf("%d", &pos[0]);
-        printf("enter col:");
-        scanf("%d", &pos[1]);
-        printf("enter x or o (1 or 0)\n");
-        scanf(" %c", &sign);
-        hasMask |= (1 << (pos[0] * 3 + pos[1]));
-        xOrOMask |= ((SignToBool(sign)) << (pos[0] * 3 + pos[1]));
+    while (!win && turns) {
+        printf("enter row:\n");
+        scanf("%hd", &pos[ZERO]);
+        printf("enter col:\n");
+        scanf("%hd", &pos[ONE]);
+        printf("enter %c or %c:\n", X_SIGN, O_SIGN);
+        hasMask |= POS_IN_MASK_TO_BITS(pos[ZERO], pos[ONE]);
+        xOrOMask |= (TURNS_TO_SIGN_BOOL(turns)) << POS_IN_MASK(pos[ZERO], pos[ONE]);
         win = isWinner(hasMask, xOrOMask);
         printBoard(hasMask, xOrOMask);
+        turns--;
     }
-    printf("%c won!!!", sign);
+    if(turns) {
+        printf("%c won!!!", BOOL_TO_SIGN(turns));
+    } else{
+        printf("tie");
+    }
 }
 
 void main() {
-    runGame();
+    runTicTacToeGame();
 }
