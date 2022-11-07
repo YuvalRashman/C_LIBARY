bbf4abebe4920f705454e031bef5e2f954fcd919
YuvalRashman <56125252+YuvalRashman@users.noreply.github.com>
2022-11-07 08:34:41 +0200

    same

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/TicTacToe.c.o"
+  "CMakeFiles/untitled.dir/TicTacToe.c.o.d"
+  "CMakeFiles/untitled.dir/main.c.o"
+  "CMakeFiles/untitled.dir/main.c.o.d"
+  "libuntitled.dll.a"
+  "untitled.exe"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
