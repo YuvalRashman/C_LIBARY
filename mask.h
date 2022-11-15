e5096c6932a571b31b1264eda73c6e548ce1f6f9
YuvalRashman <56125252+YuvalRashman@users.noreply.github.com>
2022-11-15 14:10:58 +0200

    made some fixes to copyNibbleByTimes

--- mask.h
+++ mask.h
@@ -4,7 +4,10 @@
 
 #include "definesLib.h"
 
-#define IS_ALL_BITS_OFF(mask) mask == 0
+#define FULL_NIBBLE 0xF
+#define NIBBLE_SIZE 4
+
+#define IS_ALL_BITS_OFF(mask) mask == ZERO
 #define IS_ALL_BITS_ON(mask) IS_ALL_BITS_OFF(~mask)
 #define TURN_ON_BIT(size) (ONE << (size - ONE))
 
@@ -27,9 +30,9 @@ void turnOffBit(typ *mask, unsigned short usBitIndex);
 
 void changeBit(typ *mask, unsigned short usBitIndex);
 
-int copyNibbleByLengthOfNum(int numBin, int length);
+int copyNibbleByTimes(unsigned short usNibble, unsigned short usTimes);
 
-unsigned int decimalAsHex(int num);
+unsigned int decimalAsHex(int nNum);
 
 //--------------------------------------------------------------------------------------------------------------------
 //                                                         initMask
@@ -176,33 +179,28 @@ void changeBit(typ *mask, unsigned short usBitIndex) {
 }
 
 //--------------------------------------------------------------------------------------------------------------------
-//                                                  copyNibbleByLengthOfNum
-//                                                  -----------------------
+//                                                    copyNibbleByTimes
+//                                                    -----------------
 //
 // General : this function will copy the last nibble to the nibbles before him
-// in the length that we want
+// in the usTimes that we want
 // for example:
-// numBin = 0xf    (0000 0000 0000 0000 0000 0000 0000 1111)
-// length = 3
-//
-//                 (0000 0000 0000 0000 1111 1111 1111 1111)
+// usNibble = 0xf (1111)
+// usTimes = 3
+// result = (1111 1111 1111 1111)
 //
 //
 // Parameters :
-// numBin - the number we will copy his last nibble
-// length - the length that we want to copy
+// usNibble - the number we will copy his last nibble
+// usTimes - the usTimes that we want to copy
 //
 // Return Value : the new number with the nibble
 //--------------------------------------------------------------------------------------------------------------------
-int copyNibbleByLengthOfNum(int numBin, int length) {
-    int count, shift = 4, newNumBin = numBin;
-    for (count = 0; count < length; shift += 4, count++) {
-        int tmp = numBin
-                << shift; // we move the original nibbles as the length, every time shift will grow by 4, so we go to the nibble before the last one
-        // for example: 0xf << 8
-        // 0xf00
-
-        newNumBin |= tmp;
+int copyNibbleByTimes(unsigned short usNibble, unsigned short usTimes) {
+    int newNumBin = usNibble;
+    for (; usTimes; usTimes--) {
+        newNumBin <<= NIBBLE_SIZE;
+        newNumBin += usNibble;
     }
     return newNumBin;
 }
@@ -213,31 +211,24 @@ int copyNibbleByLengthOfNum(int numBin, int length) {
 //
 // General : this function will convert a decimal number to hex number
 // this means that each decimal digit will be relocated into a nibble
+
 // for example:
-// num = 1234     ( 0000 0000 0000 0000 0000 0100 1101 0010 ) or 0x4D2
-// will return numBin:
-// numBin = 4660      ( 0000 0000 0000 0000 0001 0010 0011 0100 ) or 0x1234
-//
-// the reason I do it is that now each decimal digit is located in its own nibble
-//
+// nNum = 1234 (0100 1101 0010) or 0x4D2
+// will turn to: 4660 (0001 0010 0011 0100 ) or 0x1234
 //
 // Parameters :
-// num - the number we relocated
+// nNum - the number we relocated
 //
-// Return Value : the relocated number
+// Return Value : the relocated number / the new number
 //--------------------------------------------------------------------------------------------------------------------
-unsigned int decimalAsHex(int num) {
-    unsigned int numBin = 0;
-    typ mask = 0xf;
-
-    while ((numBin & mask) ==
-           0) // while the last nibble is bigger than 0, represent the number in hex, keep the same number
+unsigned int decimalAsHex(int nNum) {
+    unsigned int numBin = ZERO;
+    while (!(numBin & FULL_NIBBLE)) // while the last nibble is bigger than 0, represent the number in hex.
     {
-        numBin >>= 4;
-        numBin |= ((num % TEN) << 28);
-        num /= TEN;
+        numBin >>= NIBBLE_SIZE;
+        numBin |= ((nNum % TEN) << 28);
+        nNum /= TEN;
     }
-
     return numBin;
 }
 
