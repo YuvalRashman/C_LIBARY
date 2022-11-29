ba381ec9922d5f624defcb311369682d36305b51
YuvalRashman <56125252+YuvalRashman@users.noreply.github.com>
2022-11-29 23:29:17 +0200

    edit

--- stack/stack_start.c
+++ stack/stack_start.c
@@ -6,6 +6,7 @@
 
 #define STACK_SIZE 4
 #define BOOL char
+#define FALSE 0
 #define EMPTY_STACK -1 // the EOD if the stack is empty
 
 typedef struct stack stack;
@@ -13,13 +14,40 @@ typedef int typ; // the kind of value  that inside the stack.
 
 struct stack {
     typ array[STACK_SIZE];
-    short top;
+    short usTop;
 };
 
 void Init_stack(stack *stack1);
+
 typ Pop_stack(stack *stack1);
 
+void Push_stack(stack *stack1, typ value);
 
+BOOL Is_Empty_Stack(stack *stack1);
+
+void Opposite_stack(stack *stack1);
+
+void Copy_stack(stack stackToCopy, stack *copy);
+
+void Opposite_part_of_stack(stack stackToCopy, stack *copy, unsigned short usAmount);
+
+BOOL If_num_in_stack(stack stack1, typ value);
+
+unsigned short Total_items_in_stack(stack stack1);
+
+BOOL criterion(typ value);
+
+void Split_Stack(stack *stack1, stack *stack2, stack *stack3);
+
+void United_stack(stack *stack1, stack *stack2);
+
+void Empty_stack(stack *stack1);
+
+BOOL Is_Equal_Stack(stack stack1, stack stack2);
+
+unsigned short Sum_Stack(stack stack1);
+
+void Print_Stack(stack stack1);
 
 // --------------------------------------------------------------------------------------------------------------------
 //                                                         Init_stack
@@ -38,7 +66,7 @@ typ Pop_stack(stack *stack1);
 //
 //--------------------------------------------------------------------------------------------------------------------
 void Init_stack(stack *stack1) {
-    stack1->top = EMPTY_STACK;
+    stack1->usTop = EMPTY_STACK;
 }
 
 // --------------------------------------------------------------------------------------------------------------------
@@ -58,7 +86,7 @@ void Init_stack(stack *stack1) {
 //
 //--------------------------------------------------------------------------------------------------------------------
 typ Pop_stack(stack *stack1) {
-    return stack1->array[stack1->top--];;
+    return (stack1->array[(stack1->usTop--)]);;
 }
 
 
@@ -80,7 +108,7 @@ typ Pop_stack(stack *stack1) {
 //
 //--------------------------------------------------------------------------------------------------------------------
 void Push_stack(stack *stack1, typ value) {
-    stack1->array[++stack1->top] = value;;
+    stack1->array[++(stack1->usTop)] = value;;
 }
 
 // --------------------------------------------------------------------------------------------------------------------
@@ -101,7 +129,7 @@ void Push_stack(stack *stack1, typ value) {
 //
 //--------------------------------------------------------------------------------------------------------------------
 BOOL Is_Empty_Stack(stack *stack1) {
-    return stack1->top == EMPTY_STACK;
+    return stack1->usTop == EMPTY_STACK;
 }
 
 // --------------------------------------------------------------------------------------------------------------------
@@ -122,17 +150,17 @@ BOOL Is_Empty_Stack(stack *stack1) {
 //--------------------------------------------------------------------------------------------------------------------
 void Opposite_stack(stack *stack1) {
     stack temp1;
-    Init_stack(temp1);
+    Init_stack(&temp1);
     stack temp2;
-    Init_stack(temp2);
+    Init_stack(&temp2);
     while (Is_Empty_Stack(stack1)) {
         Push_stack(&temp1, Pop_stack(stack1));
     }
-    while (Is_Empty_Stack(temp1)) {
-        Push_stack(&temp2, Pop_stack(temp1));
+    while (Is_Empty_Stack(&temp1)) {
+        Push_stack(&temp2, Pop_stack(&temp1));
     }
-    while (Is_Empty_Stack(temp2)) {
-        Push_stack(&stack1, Pop_stack(temp2));
+    while (Is_Empty_Stack(&temp2)) {
+        Push_stack(stack1, Pop_stack(&temp2));
     }
 }
 
@@ -144,7 +172,7 @@ void Opposite_stack(stack *stack1) {
 //
 // Parameters :
 // stackToCopy -  the stack to copy from (In)
-// copy - the stack we want to copy to In)
+// copy - the stack we want to copy to (In)
 //
 // Runtime function: T(n) 2n + 2n +  2n = 6n --> O(n).
 //
@@ -170,7 +198,7 @@ void Copy_stack(stack stackToCopy, stack *copy) {
 // Parameters :
 // stackToCopy -  the stack to copy from (In)
 // copy - the stack we want to copy to (In)
-// amount - the amount we want to copy (In)
+// usAmount - the amount we want to copy (In)
 //
 // m - the amount to copy
 // Runtime function: T(m) 3m + 1 --> O(m).
@@ -180,11 +208,11 @@ void Copy_stack(stack stackToCopy, stack *copy) {
 // Return Value : None.
 //
 //--------------------------------------------------------------------------------------------------------------------
-void Opposite_part_of_stack(stack stackToCopy, stack *copy, unsigned short amount) {
+void Opposite_part_of_stack(stack stackToCopy, stack *copy, unsigned short usAmount) {
     Init_stack(copy);
-    while (amount > 0) {
-        Push_stack(copy, Pop_stack(stackToCopy));
-        amount--;
+    while (usAmount > 0) {
+        Push_stack(copy, Pop_stack(&stackToCopy));
+        usAmount--;
     }
 }
 
@@ -208,7 +236,7 @@ void Opposite_part_of_stack(stack stackToCopy, stack *copy, unsigned short amoun
 BOOL If_num_in_stack(stack stack1, typ value) {
     BOOL flag = FALSE;
     while (!flag && Is_Empty_Stack(&stack1)) {
-        flag = stack1.array[stack1.top--] == value;
+        flag = stack1.array[stack1.usTop--] == value;
     }
     return flag;
 }
@@ -230,12 +258,12 @@ BOOL If_num_in_stack(stack stack1, typ value) {
 //
 //--------------------------------------------------------------------------------------------------------------------
 unsigned short Total_items_in_stack(stack stack1) {
-    unsigned short count = 0;
+    unsigned short usCount = 0;
     while (Is_Empty_Stack(&stack1)) {
-        count++;
+        usCount++;
         Pop_stack(&stack1);
     }
-    return count;
+    return usCount;
 }
 
 BOOL criterion(typ value) {
@@ -266,7 +294,7 @@ void Split_Stack(stack *stack1, stack *stack2, stack *stack3) {
     typ temp;
     while (Is_Empty_Stack(stack1)) {
         temp = Pop_stack(stack1);
-        criterion(typ) ? Push_stack(stack2, temp) : Push_stack(stack1, temp);
+        criterion(temp) ? Push_stack(stack2, temp) : Push_stack(stack1, temp);
     }
 }
 
@@ -274,14 +302,13 @@ void Split_Stack(stack *stack1, stack *stack2, stack *stack3) {
 //                                                         United_stack
 //                                                         ------------
 //
-// General : The function split the stack to two stack according to the criterion().
+// General : The function unite two stacks to one save in the left stack.
 //
 // Parameters :
-// stack1 - the stack to split (In)
-// stack2 - the values that sustain the criterion()  (In)
-// stack3 - the values that doesn't sustain the criterion()  (In)
+// stack1 - the first stack and the stack where the result will be save (In)
+// stack2 - the second stack()  (In)
 //
-// Runtime function: T(n) 4n + 2 --> O(n).
+// Runtime function: T(n) 2n --> O(n).
 //
 // nesting level : 1
 //
@@ -294,36 +321,95 @@ void United_stack(stack *stack1, stack *stack2) {
     }
 }
 
+// --------------------------------------------------------------------------------------------------------------------
+//                                                         Empty_stack
+//                                                         ------------
+//
+// General : The function empty the given stack.
+//
+// Parameters :
+// stack1 - the stack to empty (In)
+//
+// Runtime function: None.
+//
+// nesting level : 0
+//
+// Return Value : None.
+//
+//--------------------------------------------------------------------------------------------------------------------
 void Empty_stack(stack *stack1) {
     Init_stack(stack1);
 }
 
-void Is_Equal_Stack(stack stack1, stack stack2) {
+// --------------------------------------------------------------------------------------------------------------------
+//                                                         Is_Equal_Stack
+//                                                         --------------
+//
+// General : The function check if two stacks equal.
+//
+// Parameters :
+// stack1 - the first stack (In)
+// stack2 - the second stack (In)
+//
+// Runtime function: 3n + 2 --> O(n).
+//
+// nesting level : 1
+//
+// Return Value : 1 if the stacks are equal, 0 if not.
+//
+//--------------------------------------------------------------------------------------------------------------------
+BOOL Is_Equal_Stack(stack stack1, stack stack2) {
     BOOL flag;
     flag = Total_items_in_stack(stack1) == Total_items_in_stack(stack2);
-    while (flag && Is_Empty_Stack(stack1)) {
-        flag = Pop_stack(stack1) == Pop_stack(stack2);
+    while (flag && Is_Empty_Stack(&stack1)) {
+        flag = Pop_stack(&stack1) == Pop_stack(&stack2);
     }
     return flag;
 }
 
+// --------------------------------------------------------------------------------------------------------------------
+//                                                         Is_Equal_Stack
+//                                                         --------------
+//
+// General : The function sum all values in the stack.
+//
+// Parameters :
+// stack1 - the stack to sum (In)
+//
+// Runtime function: 2n + 2 --> O(n).
+//
+// nesting level : 1
+//
+// Return Value : the sum.
+//
+//--------------------------------------------------------------------------------------------------------------------
 unsigned short Sum_Stack(stack stack1) {
-    unsigned short sum = 0;
-    while (Is_Empty_Stack(stack1)) {
-        sum = Pop_stack(stack1);
+    unsigned short usSum = 0;
+    while (Is_Empty_Stack(&stack1)) {
+        usSum = Pop_stack(&stack1);
     }
-    return sum;
+    return usSum;
 }
 
+// --------------------------------------------------------------------------------------------------------------------
+//                                                         Is_Equal_Stack
+//                                                         --------------
+//
+// General : The function print the stack.
+//
+// Parameters :
+// stack1 - the stack to print (In)
+//
+// Runtime function: 8n --> O(n).
+//
+// nesting level : 1.
+//
+// Return Value : None.
+//
+//--------------------------------------------------------------------------------------------------------------------
 void Print_Stack(stack stack1) {
-    unsigned short items = Total_items_in_stack(stack1);
-    for (; items > 0; items--) {
-        printf("%d", Pop_stack(stack1));
+    Opposite_stack(&stack1);
+    while (Is_Empty_Stack(&stack1)) {
+        printf("%d", Pop_stack(&stack1));
     }
 }
-
-unsigned short getMiddleValue(stack stack1) {
-    while ((temp = Pop_stack(stack1)) < Pop_stack(stack1));
-    return temp;
-}
-
