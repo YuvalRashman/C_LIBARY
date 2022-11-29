b1fdf810a170e2b16d4a91c98f0ab54d64cc71db
YuvalRashman <56125252+YuvalRashman@users.noreply.github.com>
2022-11-29 21:00:29 +0200

    save

--- stack/stack_start.c
+++ stack/stack_start.c
@@ -5,6 +5,7 @@
 #define STACK_SIZE 4
 
 typedef struct stack stack;
+typedef int typ;
 
 struct stack {
     typ array[STACK_SIZE];
@@ -20,3 +21,18 @@ typ Pop_stack(stack *stack1) {
 }
 
 
+typ Push_stack(stack *stack1, typ value) {
+    stack1->array[++stack1->top] = value;;
+}
+
+BOOL Is_Empty_Stack(stack *stack1) {
+    return stack1->top == -1;
+}
+
+typ stack_Opposite(stack *stack1) {
+    stack temp1;
+    stack temp2;
+    while (Is_Empty_Stack(stack1)) {
+        Push_stack(&temp1, Pop_stack(stack1));
+    }
+}
